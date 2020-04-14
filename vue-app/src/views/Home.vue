86c52e8585f55e73d99b414786256e52c88edc9c
kasprzyk <piotkas1@gmail.com>
2020-04-14 14:26:21 +0200

    configuration

--- vue-app/src/views/Home.vue
+++ vue-app/src/views/Home.vue
@@ -7,12 +7,13 @@
 
 <script>
 // @ is an alias to /src
-import HelloWorld from "@/components/HelloWorld.vue";
-
+import HelloWorld from '@/components/HelloWorld.vue'
+import EventCard from '@/components/EventCard.vue'
 export default {
-  name: "Home",
+  name: 'Home',
   components: {
-    HelloWorld
+    HelloWorld,
+    EventCard
   }
-};
+}
 </script>
