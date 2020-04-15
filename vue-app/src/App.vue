ef4c7ce8766195a3ed83bfd6481eeedf0e06b7d3
kasprzyk <piotkas1@gmail.com>
2020-04-15 21:14:32 +0200

    Router links added

--- vue-app/src/App.vue
+++ vue-app/src/App.vue
@@ -1,8 +1,9 @@
 <template>
   <div id="app">
     <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
+      <router-link to="/">Event List</router-link> |
+      <router-link to="/event-create">Event show</router-link> |
+      <router-link to="/event-show">Event create</router-link>
     </div>
     <router-view />
   </div>
