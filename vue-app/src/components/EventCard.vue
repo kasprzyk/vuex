aa14c41a9d2666c504460d9f1109c1f381175946
kasprzyk <piotkas1@gmail.com>
2020-04-15 23:18:00 +0200

    EventCard component added

---
+++ vue-app/src/components/EventCard.vue
@@ -0,0 +1,20 @@
+<template>
+  <div>
+    <h4>{{ title }}</h4>
+  </div>
+</template>
+<script>
+export default {
+  name: 'EventCard',
+  data() {
+    return {
+      title: 'Park cleanup'
+    }
+  }
+}
+</script>
+<style scoped>
+h4 {
+  color: green;
+}
+</style>
--- vue-app/src/views/EventList.vue
+++ vue-app/src/views/EventList.vue
@@ -1,8 +1,19 @@
 <template>
   <div>
-    <h1>Event Listing</h1>
+    <h1>Events Listing</h1>
+    <EventCard />
     <router-link :to="{ name: 'event-show', params: { id: '123' } }"
       >Show event</router-link
     >
   </div>
 </template>
+
+<script>
+import EventCard from '@/components/EventCard.vue'
+export default {
+  name: 'EventList',
+  components: {
+    EventCard
+  }
+}
+</script>
