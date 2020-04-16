60310f1e16d6bddca261411895aab0bb1ea84996
kasprzyk <piotkas1@gmail.com>
2020-04-16 14:31:27 +0200

    Global registration of new component

--- vue-app/src/main.js
+++ vue-app/src/main.js
@@ -1,12 +1,13 @@
-import Vue from "vue";
-import App from "./App.vue";
-import router from "./router";
-import store from "./store";
+import Vue from 'vue'
+import App from './App.vue'
+import router from './router'
+import store from './store'
+import BaseIcon from '@/components/BaseIcon.vue'
 
-Vue.config.productionTip = false;
-
+Vue.config.productionTip = false
+Vue.component('BaseIcon', BaseIcon)
 new Vue({
   router,
   store,
   render: h => h(App)
-}).$mount("#app");
+}).$mount('#app')
