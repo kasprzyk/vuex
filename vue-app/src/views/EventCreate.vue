4d14248ca86650a99c2a067815f9afdfe9b0b921
kasprzyk <piotkas1@gmail.com>
2020-04-17 15:57:34 +0200

    Using store, state

--- vue-app/src/views/EventCreate.vue
+++ vue-app/src/views/EventCreate.vue
@@ -1,3 +1,16 @@
 <template>
-  <h1>Create Event</h1>
+  <div>
+    <h1>Create an event, {{ userName }}</h1>
+  </div>
 </template>
+<script>
+export default {
+  computed: {
+    userName() {
+      return this.$store.state.user.name
+    }
+  }
+}
+</script>
+
+<style scoped></style>
