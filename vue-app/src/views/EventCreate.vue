a83833190f5714e4f766630a629401ec5d97fc6f
kasprzyk <piotkas1@gmail.com>
2020-04-17 16:20:59 +0200

    using getters

--- vue-app/src/views/EventCreate.vue
+++ vue-app/src/views/EventCreate.vue
@@ -1,16 +1,21 @@
 <template>
   <div>
-    <h1>Create an event, {{ userName }}</h1>
+    <h1>Create an event, {{ user.name }}</h1>
+    <p>Event was created by {{ user.id }}</p>
+    <ul>
+      <li v-for="category in categories" :key="category">{{ category }}</li>
+    </ul>
   </div>
 </template>
 <script>
 import { mapState } from 'vuex'
 export default {
-  computed: mapState({
-    userName: state => state.user.name,
-    userId: state => state.user.id,
-    categories: state => state.categories
-  })
+  computed: {
+    categoriesLength() {
+      return this.$store.getters.categoriesLength
+    },
+    ...mapState(['user', 'categories'])
+  }
 }
 </script>
 
