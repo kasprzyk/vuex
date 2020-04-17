151bb029d69729f9c70a3d7cca63dcd9161d6fe4
kasprzyk <piotkas1@gmail.com>
2020-04-17 18:36:12 +0200

    Form and datepicker added

--- vue-app/src/views/EventCreate.vue
+++ vue-app/src/views/EventCreate.vue
@@ -3,6 +3,50 @@
     <h1>Create an event, {{ user.name }}</h1>
     <p>Event was created by {{ user.id }}</p>
     <p>{{ getEvent(1) }}</p>
+    <form>
+      <label>Select a category</label>
+      <select v-model="event.category">
+        <option v-for="cat in categories" :key="cat">{{ cat }}</option>
+      </select>
+      <h3>Name & describe your event</h3>
+      <div class="field">
+        <label>Title</label>
+        <input
+          v-model="event.title"
+          type="text"
+          placeholder="Add an event title"
+        />
+      </div>
+      <div class="field">
+        <label>Description</label>
+        <input
+          v-model="event.description"
+          type="text"
+          placeholder="Add a description"
+        />
+      </div>
+      <h3>Where is your event?</h3>
+      <div class="field">
+        <label>Location</label>
+        <input
+          v-model="event.location"
+          type="text"
+          placeholder="Add a location"
+        />
+      </div>
+      <h3>When is your event?</h3>
+      <div class="field">
+        <label>Date</label>
+        <DatePicker v-model="event.date" placeholder="Select a date" />
+      </div>
+      <div class="field">
+        <label>Select a time</label>
+        <select v-model="event.time">
+          <option v-for="time in times" :key="time">{{ time }}</option>
+        </select>
+      </div>
+      <input type="submit" class="button -fill-gradient" value="Submit" />
+    </form>
     <ul>
       <li v-for="category in categories" :key="category">{{ category }}</li>
     </ul>
@@ -10,7 +54,11 @@
 </template>
 <script>
 import { mapState, mapGetters } from 'vuex'
+import DatePicker from 'vuejs-datepicker'
 export default {
+  components: {
+    DatePicker
+  },
   computed: {
     ...mapGetters(['getEventById']),
     ...mapState(['user', 'categories'])
