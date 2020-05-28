2b3317ed7e26a648a3d4c94a534145984fe0419f
kasprzyk <piotkas1@gmail.com>
2020-05-28 22:24:14 +0200

    Adding props and error handling

--- vue-app/src/router/index.js
+++ vue-app/src/router/index.js
@@ -35,20 +35,24 @@ const routes = [
     props: true,
     beforeEnter(routeTo, routeFrom, next) {
       // before this route is loaded
-      store.dispatch('event/fetchEvent', routeTo.params.id).then(event => {
-        routeTo.params.event = event
-        next()
-      })
+      store
+        .dispatch('event/fetchEvent', routeTo.params.id)
+        .then(event => {
+          routeTo.params.event = event
+          next()
+        })
+        .catch(() => next({ name: '404', params: { resource: 'event' } }))
     }
   },
   {
     path: '/404',
     name: '404',
-    component: NotFound
+    component: NotFound,
+    props: true
   },
   {
     path: '*',
-    redirect: { name: '404' }
+    redirect: { name: '404', params: { resource: 'page' } }
   }
 ]
 
