d0281883f2510b582c782b9183b5d93b5ced3b31
kasprzyk <piotkas1@gmail.com>
2020-05-29 14:16:57 +0200

    BaseSelect added

---
+++ vue-app/src/components/BaseSelect.vue
@@ -0,0 +1,20 @@
+<template>
+  <div>
+    <label v-if="label">{{ label }}</label>
+    <input :id="label" :value="value" v-bind="$attrs" @input="updateValue" />
+  </div>
+</template>
+<script>
+export default {
+  inheritAttrs: false,
+  props: {
+    value: [String, Number],
+    label: String
+  },
+  methods: {
+    updateValue(event) {
+      this.$emit('input', event.target.value)
+    }
+  }
+}
+</script>
